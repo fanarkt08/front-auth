1d73104171dfc9d1266f7ca578f1c2892beab020
fana-rkt <fana@n-vibe.fr>
2025-07-23 11:03:50 +0200

    update logout

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,22 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router';
 
 const Logout = () => {
   const navigate = useNavigate();
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     const handleLogout = async () => {
       const auth = JSON.parse(localStorage.getItem("auth"));
+      const token = auth?.token;
 
-      if (auth?.token) {
-        try {
+      try {
+        if (token) {
           const response = await fetch('https://offers-api.digistos.com/api/auth/logout', {
             method: 'POST',
             headers: {
-              'Content-Type': 'application/json',
               Accept: 'application/json',
-              Authorization: `Bearer ${auth.token}`,
+              Authorization: `Bearer ${token}`,
             },
           });
 
@@ -24,15 +23,13 @@ const Logout = () => {
             const data = await response.json();
             throw new Error(data.message || "Erreur lors de la déconnexion");
           }
-
-        } catch (err) {
-          console.error("Erreur lors de la déconnexion :", err.message || err);
-          setError("Une erreur est survenue lors de la déconnexion.");
         }
+      } catch (err) {
+        console.error("Erreur lors de la déconnexion :", err.message || err);
+      } finally {
+        localStorage.removeItem("auth");
+        navigate("/connexion");
       }
-
-      localStorage.removeItem("auth");
-      navigate("/connexion");
     };
 
     handleLogout();
