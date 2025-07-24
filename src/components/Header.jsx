f0c402076371b9d55c0dae654f88e89f2d45bb4a
fana-rkt <fana@n-vibe.fr>
2025-07-24 11:43:27 +0200

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { Nav, Navbar, Container } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 
